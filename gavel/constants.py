b3e1b6a033d123882ebbe2ef650b62ee090a6bc2
Gareth Lau <gareth.kh.lau@gmail.com>
2021-11-20 12:25:40 -0500

    Rename hack western 7 to hack western 8

--- gavel/constants.py
+++ gavel/constants.py
@@ -1,12 +1,12 @@
 ANNOTATOR_ID = 'annotator_id'
 TELEMETRY_URL = 'https://telemetry.anish.io/api/v1/submit'
-TELEMETRY_DELTA = 20 * 60 # seconds
+TELEMETRY_DELTA = 20 * 60  # seconds
 SENDGRID_URL = "https://api.sendgrid.com/v3/mail/send"
 
 # Setting
 # keys
-SETTING_CLOSED = 'closed' # boolean
-SETTING_TELEMETRY_LAST_SENT = 'telemetry_sent_time' # integer
+SETTING_CLOSED = 'closed'  # boolean
+SETTING_TELEMETRY_LAST_SENT = 'telemetry_sent_time'  # integer
 # values
 SETTING_TRUE = 'true'
 SETTING_FALSE = 'false'
@@ -33,12 +33,12 @@ Gavel makes it really simple for you to submit votes, but please think hard
 before you vote. **Once you make a decision, you can't take it back**.
 '''.strip()
 
-DEFAULT_EMAIL_SUBJECT = 'Welcome to Gavel - Hack Western 7 Judging Platform'
+DEFAULT_EMAIL_SUBJECT = 'Welcome to Gavel - Hack Western 8 Judging Platform'
 
 DEFAULT_EMAIL_BODY = '''
 Hi {name},
 
-Welcome to Gavel, the online expo judging system for Hack Western 7. This email contains your
+Welcome to Gavel, the online expo judging system for Hack Western 8. This email contains your
 magic link to the judging system.
 
 DO NOT SHARE this email with others, as it contains your personal magic link.
@@ -52,7 +52,7 @@ If you have any questions or run into any issues, please contact a Hack Western 
 
 Best,
 
-Hack Western 7 Organizing Team
+Hack Western 8 Organizing Team
 
 '''.strip()
 
